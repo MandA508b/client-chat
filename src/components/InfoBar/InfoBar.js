0da20ac3d62dc8201006a9c3460213de145b5e7a
MandA508b <123251306+MandA508b@users.noreply.github.com>
2023-03-07 17:54:56 +0200

    edits

--- src/components/InfoBar/InfoBar.js
+++ src/components/InfoBar/InfoBar.js
@@ -1,22 +1,19 @@
 /* eslint-disable */
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import onlineIcon from '../../icons/onlineIcon.png';
-import closeIcon from '../../icons/closeIcon.png';
-import axios from 'axios'
 import './InfoBar.css';
-import { useLocation } from 'react-router-dom';
-import queryString from 'query-string';
 
 
-const InfoBar = ({ room }) =>{
+const InfoBar = () =>{
 
   return(
     <div className="infoBar">
-      <div className="leftInnerContainer">
         <img className="onlineIcon" src={onlineIcon} alt="online icon" />
-      </div>
+         <span>
+             ⇑потягніть вгору, щоб написати
+         </span>
 
     </div>
 );
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
 import './Input.css';
-
+import SendIcon from './../../icons/iconsend.png'
 const Input = ({ setMessage, sendMessage, message }) => (
   <form className="form">
     <input
       className="input"
       type="text"
-      placeholder="Type a message..."
+      placeholder="Напишіть повідомлення..."
       value={message}
       onChange={({ target: { value } }) => setMessage(value)}
       onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
     />
-    <button className="sendButton" onClick={e => sendMessage(e)}>Send</button>
+    <button className="sendButton" onClick={e => sendMessage(e)}><img src={SendIcon} alt=""/></button>
   </form>
 )
 

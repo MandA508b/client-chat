1e155e5e51920a3ab0b8e81e94bbdf4bcb3c978d
MandA508b <123251306+MandA508b@users.noreply.github.com>
2023-03-12 23:38:58 +0200

    changed access3

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -30,11 +30,12 @@ const Chat = ( ) => {
       try{
         const {advertisementId,chatId } = queryString.parse(location.search);
         const adv = await axios.post('https://liga-bot.telegram-crm.work/advertisement/findById',{advertisementId})
+        setAccessErr(JSON.stringify(adv, null, 4))
         if(adv.advertisement.statusStage === 'open') setAccess(true)
         else if(adv.advertisement.linkedChat === chatId) setAccess(true)
       }catch (e){
         setAccess(false)
-        setAccessErr(JSON.stringify(e, null, 4))
+        //setAccessErr(JSON.stringify(e, null, 4))
       }
       
     }
